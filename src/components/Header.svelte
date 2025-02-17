48d9df34a34a0bfda4bd5e82b9884fb497333bd0
Chandrashekar <chandrashekar31.v@gmail.com>
2025-02-17 22:41:43 +0530

    commented unwanted headers

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -19,7 +19,7 @@
   <span class="justify-center items-center md:ml-20 ml-2 md:flex hidden"
     ><a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">What we do</a></span
   >
-  <span class="flex justify-center items-center relative group md:ml-20 ml-2">
+  <!-- <span class="flex justify-center items-center relative group md:ml-20 ml-2">
     <button class=" hover:text-gray-400"> Services </button>
     <div
       class="absolute left-0 hidden space-y-2 mt-48 bg-white text-black shadow-lg group-hover:block rounded-md w-48 p-2"
@@ -48,7 +48,7 @@
       <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">App Development</a>
       <a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">SEO Optimization</a>
     </div>
-  </span>
+  </span> -->
   <span class="flex justify-center items-center md:ml-20 ml-2"
     ><a href="#" class="block md:px-4 md:py-2 px-2 py-1 hover:bg-gray-100">Industries</a></span
   >
