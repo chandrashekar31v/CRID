60497e03b55dac40191d086cd6de492f75e2c54b
CIRDtechinsols <CIRDtechinsols@gmail.com>
2025-02-17 15:34:39 +0530

    Update ContactUs.svelte

--- src/components/ContactUs.svelte
+++ src/components/ContactUs.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <div class="pt-10 bg-slate-200">
-    <ImageTextBanner image={contact} imagePosition="right" data={{title:"CONTACT US",description:"<b>Email</b>: CRIDtechinsols@gmail.com <br><b>Phone</b>: 7382505318 <br><b>Address</b>: H.No-206, Sohan RNR Orchid,<br>Doddenakondi, Ferns City, Bangalore - 560037<br>"}} className="bg-slate-200" />
+    <ImageTextBanner image={contact} imagePosition="right" data={{title:"CONTACT US",description:"<b>Email</b>: CIRDtechinsols@gmail.com <br><b>Phone</b>: 7382505318 <br><b>Address</b>: H.No-206, Sohan RNR Orchid,<br>Doddenakondi, Ferns City, Bangalore - 560037<br>"}} className="bg-slate-200" />
     <!-- <input type='string' /><button on:click={}>Send</button> -->
 </div>
